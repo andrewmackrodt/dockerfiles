9e44d6a546b3c61ccedf6123ba77e4befa9404fc
Andrew Mackrodt <andrew@denimsoft.com>
2019-05-29 23:40:21 +0100

    Build multiple php and nodejs versions

--- Makefile.mk
+++ Makefile.mk
@@ -1,36 +1,8 @@
 SHELL=/bin/bash
 
-# registry args
-REGISTRY_HOST=docker.io
-USERNAME=andrewmackrodt
+VENDOR=andrewmackrodt
 
 SUPPORT_FUNCTIONS := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))/.make-support
-
-# image args
-NAME=$(shell basename $(CURDIR))
-IMAGE=$(REGISTRY_HOST)/$(USERNAME)/$(NAME)
-VERSION=$(shell . $(SUPPORT_FUNCTIONS) ; getVersion)
-VERSION_MAJOR_MINOR=$(shell . $(SUPPORT_FUNCTIONS) ; getVersionMajorMinor)
-VERSION_MAJOR=$(shell . $(SUPPORT_FUNCTIONS) ; getVersionMajor)
-
-# VCS LABEL build args
-VCS_NAME=$(shell basename $$(dirname $(abspath $(lastword $(MAKEFILE_LIST)))))
-VCS_HOST=github.com
-VCS_URL=https://$(VCS_HOST)/$(USERNAME)/$(VCS_NAME)
-VCS_REF=$(shell . $(SUPPORT_FUNCTIONS) ; getVcsRef)
-
-DOCKER_BUILD_ARGS_LABEL=\
-	--build-arg "LABEL_NAME=$(NAME)" \
-	--build-arg "VERSION=$(VERSION)" \
-	--build-arg "LABEL_DESCRIPTION=" \
-	--build-arg "LABEL_USAGE=https://$(VCS_HOST)/$(USERNAME)/$(VCS_NAME)/tree/master/$(NAME)" \
-	--build-arg "LABEL_VENDOR=$(USERNAME)" \
-	--build-arg "LABEL_VCS_URL=$(VCS_URL)" \
-	--build-arg "LABEL_VCS_REF=$(VCS_REF)"
-
-DOCKER_BUILD_ARGS=$(shell . $(SUPPORT_FUNCTIONS) ; getBuildArgs)
-DOCKER_BUILD_CONTEXT=.
-DOCKER_FILE_PATH=Dockerfile
 
 .PHONY: build pre-build do-build post-build \
 	    push pre-push do-push post-push \
@@ -40,16 +12,26 @@ build: pre-build do-build post-build
 
 pre-build:
 
-do-build: build.properties
-	docker build \
-		$(DOCKER_BUILD_ARGS_LABEL) \
-		$(DOCKER_BUILD_ARGS) \
-	    -t $(IMAGE):$(VERSION_MAJOR) \
-	    -t $(IMAGE):$(VERSION_MAJOR_MINOR) \
-	    -t $(IMAGE):$(VERSION) \
-	    -t $(IMAGE):latest \
-	    $(DOCKER_BUILD_CONTEXT) \
-	    -f $(DOCKER_FILE_PATH)
+do-build:
+	# export environment
+	export DOCKER_BUILD_CONTEXT="$(DOCKER_BUILD_CONTEXT)" ; \
+	export DOCKER_FILE_PATH="$(DOCKER_FILE_PATH)" ; \
+	export IMAGE="$(IMAGE)" ; \
+	export NAME="$(NAME)" ; \
+	export REGISTRY_HOST="$(REGISTRY_HOST)" ; \
+	export TAG_MAJOR="$(TAG_MAJOR)" ; \
+	export TAG_MINOR="$(TAG_MINOR)" ; \
+	export VCS_NAME="$(VCS_NAME)" ; \
+	export VCS_HOST="$(VCS_HOST)" ; \
+	export VCS_URL="$(VCS_URL)" ; \
+	export VCS_REF="$(VCS_REF)" ; \
+	export VENDOR="$(VENDOR)" ; \
+	\
+	# import shell helper \
+	. $(SUPPORT_FUNCTIONS) ; \
+	\
+	# execute docker build \
+	dockerBuild ; \
 
 post-build:
 
@@ -57,14 +39,19 @@ push: pre-push do-push post-push
 
 pre-push:
 
-do-push: build.properties
-	docker push $(IMAGE):$(VERSION)
-	docker push $(IMAGE):latest
+do-push:
+	# export environment
+	export IMAGE="$(IMAGE)" ; \
+	export TAG_MAJOR="$(TAG_MAJOR)" ; \
+	export TAG_MINOR="$(TAG_MINOR)" ; \
+	\
+	# import shell helper \
+	. $(SUPPORT_FUNCTIONS) ; \
+	\
+	# execute docker push \
+	dockerPush ; \
 
 post-push:
 
 clean:
-	rm -f build.properties
-
-build.properties:
-	$(shell . $(SUPPORT_FUNCTIONS) ; configure)
+	rm -f build*.properties
